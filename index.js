c24c4914e8d2f353add0131242d0bc98fefa78ab
Mark Tse <mark.tse@neverendingqs.com>
2020-11-29 22:02:26 -0500

    feat: now logs if no stack policy is found.

--- index.js
+++ index.js
@@ -3,13 +3,17 @@
 class StackPolicyByResourceType {
   constructor(serverless) {
     this.provider = serverless.getProvider('aws');
-    this.serverless = serverless.service;
+    this.serverless = serverless;
     this.hooks = {
       'before:package:finalize': this.lookupLogicalResourceIds.bind(this)
     };
   }
 
   lookupLogicalResourceIds() {
+    if(!this.serverless.service.provider.stackPolicy) {
+      this.serverless.cli.log(`'serverless-stack-policy-by-resource-type' did not find a stack policy.`);
+      return;
+    }
   }
 }
 
